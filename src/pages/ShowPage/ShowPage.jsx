9d61ad742093395cf6f2574872a086bbccc60e81
FedeBerbara <feedee96@gmail.com>
2020-11-24 01:30:46 -0300

    Se sube fetch con datos llegando OK a consola

--- src/pages/ShowPage/ShowPage.jsx
+++ src/pages/ShowPage/ShowPage.jsx
@@ -13,55 +13,49 @@ import Buttons from "./components/Buttons";
 import Footer from "../Footer/Footer";
 
 //* Services
-//import { getFullRecitalApi } from '../../services/showService';
+// import { getFullRecitalApi } from '../../services/showService';
 
 function ShowPage () {
 
     const { id } = useParams();
-    console.log(id);
 
     const [ recitalData, setRecitalData ] = useState([]);
 
     useEffect(() => {
+        const obtenerRecital =  () => {
+            fetch(`http://localhost:5000/api/obtenerFullRecital/${id}`)
+            .then(data=>data.json())
+            .then(response=>{
+                console.log(response)
+                setRecitalData(response)
+            });
+        }
         obtenerRecital();
-      }, []);
-
-      const obtenerRecital = async () => {
-          const data = await fetch(`http://localhost:5000/api/obtenerFullRecital/${id}`);
-          const fullRecital = await data.json()
-          setRecitalData(fullRecital)
-      }
-      console.log(setRecitalData);
+    }, [id]);
       
     return (
         <>
             <Header/>
                 <div className="container-fluid">
                     <div className="row">
-
-                            <div className="col-12 col-lg-10">
-                                <CardRecital imagen={recitalData.bannerRecital}/>
-                            </div>
-
-                        <div className="col-12 col-lg-8 contenedorInformacion">
-
-                                <div className="row">
-                                    <div className="col-12 col-lg-7">
-                                        <CardInformacion descripcion={recitalData.descripcion}/> 
-                                    </div>
-                                    <div className="col-12 col-lg-3">
-                                        <PerfilBanda imagen={recitalData.imgBanda}
-                                            nombre={recitalData.nombre}
-                                            genero={recitalData.genero}/>
-                                    </div>
-                                </div>
-
+                        <div className="col-12 col-lg-10">
+                            <CardRecital imagen={recitalData.bannerRecital}/>
                         </div>
-
-                            <div className="col-12 col-lg-2">
-                                <Buttons links={recitalData.linkEntradas}/>
+                        <div className="col-12 col-lg-8 contenedorInformacion">
+                            <div className="row">
+                                <div className="col-12 col-lg-7">
+                                    <CardInformacion descripcion={recitalData.descripcion}/>
+                                </div>
+                                <div className="col-12 col-lg-3">
+                                    <PerfilBanda imagen={recitalData.imgBanda}
+                                        nombre={recitalData.nombre}
+                                        genero={recitalData.genero}/>
+                                </div>
                             </div>
-
+                        </div>
+                        <div className="col-12 col-lg-2">
+                            <Buttons links={recitalData.linkEntradas}/>
+                        </div>
                     </div>               
                 </div>
             <Footer/>
